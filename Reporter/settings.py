68a0a5cc458a8b96b55fc16daa665d97666183bd
I-am-ZiHao <e64031025@gmail.com>
2023-03-25 01:37:30 +0800

    add RENDER_EXTERNAL_HOSTNAME in settings

--- Reporter/settings.py
+++ Reporter/settings.py
@@ -29,13 +29,16 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # DEBUG = True
 DEBUG = False
 
-# needed for deployment
+# needed for deploy
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # ALLOWED HOSTS
-ALLOWED_HOSTS = [
-    "reporterlinebot.herokuapp.com",
-]
+ALLOWED_HOSTS = []
+
+# needed for deploy
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # DB
 MONGO_DB = os.environ.get("MONGO_DB")
